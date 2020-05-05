73f0a5e03853c842e2dc7936f35a2ea03918329f
Harriet Goddard <goddardhe@gmail.com>
2020-05-05 18:48:48 +0100

    HTML minification up and running

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
 const gulp = require('gulp');
+let htmlmin = require('gulp-htmlmin');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const uglify = require('gulp-uglify-es').default;
 const concat = require('gulp-concat');
+
+/**
+ * HTML
+ */
+//minify HTML from src/ to dist/ for publishing
+gulp.task('html-min', () => {
+    return gulp.src('src/*.html')
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest('dist'));
+  });
 
 /**
  * STYLES
