57273557e6abd8366f93ed077a4db04cc1aeba91
Harriet Goddard <goddardhe@gmail.com>
2020-05-07 22:47:23 +0100

    main title wipe completed

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,15 +3,22 @@
     console.log("hey haz javascript is working");
 
     let wiper = d.getElementById("wiper");
+    let mainTitle = d.getElementById("mainTitle");
 
     w.addEventListener("scroll", () => {
 
         if(window.pageYOffset > 0) {
             wiper.style.width = "calc(100% - 2 * var(--border)";
             wiper.style.transition = "1.5s"; //can we combine the transitions?
+
+            mainTitle.style.right = "2.7%";
+            mainTitle.style.transition = "1.5s";
         } else {
             wiper.style.width = "calc(50% - var(--border)";
             wiper.style.transition = "1.5s";
+
+            mainTitle.style.right = "50%";
+            mainTitle.style.transition = "50%";
         }
 
     })
