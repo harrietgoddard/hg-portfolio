676429cbf0917fbebfdb391854d9b115199b253f
Harriet Goddard <goddardhe@gmail.com>
2020-05-10 12:50:27 +0100

    hero transitions rectified

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,82 +2,86 @@
 
     console.log("hey haz javascript is working");
 
+    let header = d.getElementById("header");
+    let hero = d.getElementById("hero");
     let wiper = d.getElementById("wiper");
-    let mainTitle = d.getElementById("mainTitle");
     let preTitle = d.getElementById("preTitle");
+    let mainTitle = d.getElementById("mainTitle");
     let intro = d.getElementById("intro");
     let scroll = d.getElementById("scroll");
-    let hero = d.getElementById("hero");
-    let header = d.getElementById("header");
+
+    let setStyle = (element, propertyObject) => {
+        for (property in propertyObject) {
+            element.style[property] = propertyObject[property];
+        }
+    }
+
+    let elastisize = (elements) => {
+        elements.forEach(element => setStyle(element, {"transition-timing-function": "cubic-bezier(0.7, 0, 0.3, 1)"}));
+    }
 
     w.addEventListener("scroll", () => {
 
         if(w.pageYOffset > 0) {
-            wiper.style.width = "calc(100% - 2 * var(--border)";
-            wiper.style.transition = "1.10s"; //can we combine the transitions?
-            wiper.style.transitionTimingFunction = "cubic-bezier(0.7, 0, 0.3, 1)";
 
-            mainTitle.style.right = "calc(0.17 * var(--border))";
-            mainTitle.style.transition = "1s"; //better to put this in css?
-            mainTitle.style.transitionTimingFunction = "cubic-bezier(0.7, 0, 0.3, 1)";
+            setStyle(wiper, {
+                "width": "calc(100% - 2 * var(--border)",
+                "transition": "1.10s",
+            });
 
-            scroll.style.right = "calc(0.8 * var(--border))";
-            scroll.style.transition = "1s";
-            scroll.style.transitionTimingFunction = "cubic-bezier(0.7, 0, 0.3, 1)";
+            setStyle(preTitle, {
+                "top": "-5%",
+                "transition" : "1s",
+            });
 
-            preTitle.style.top = "-5%";
-            preTitle.style.transition = "1s";
-            preTitle.style.transitionTimingFunction = "cubic-bezier(0.7, 0, 0.3, 1)";
+            setStyle(mainTitle, {
+                "right": "calc(0.17 * var(--border))",
+                "transition" : "1s",
+            });
+            
+            setStyle(intro, {
+                "opacity": "1",
+                "transition" : "3s",
+                "transitionDelay" : "1s"
+            });
 
-            intro.style.opacity = "1";
-            intro.style.transition = "1.5s";
-            intro.style.transitionDelay = "1s";
+            setStyle(scroll, {
+                "right": "calc(0.8 * var(--border))",
+                "transition" : "1s",
+            });
+
+            elastisize([wiper, mainTitle, preTitle, scroll]);
 
         } else {
-            wiper.style.width = "calc(50% - var(--border)";
-            wiper.style.transition = "1.10s";
-            wiper.style.transitionTimingFunction = "cubic-bezier(0.7, 0, 0.3, 1)";
-
-            mainTitle.style.right = "50%";
-            mainTitle.style.transition = "1s";
-            mainTitle.style.transitionTimingFunction = "cubic-bezier(0.7, 0, 0.3, 1)";
-
-            scroll.style.right = "49%";
-            scroll.style.transition = "1s";
-            scroll.style.transitionTimingFunction = "cubic-bezier(0.7, 0, 0.3, 1)";
-
-            preTitle.style.top = "16%";
-            preTitle.style.transition = "1s";
-            preTitle.style.transitionTimingFunction = "cubic-bezier(0.7, 0, 0.3, 1)";
-
-            intro.style.opacity = "0";
+            wiper.style.width = "";
+            mainTitle.style.right = "";
+            scroll.style.right = "";
+            preTitle.style.top = "";
+            intro.style.opacity = "";
             intro.style.transition = "1s";
         }
 
     })
     
     w.addEventListener("scroll", () => {
-        let trigger = hero.scrollHeight + (header.scrollHeight * 2) - w.innerHeight;
-        console.log(w.pageYOffset);
-        console.log(trigger);
+        //window pageYOffset point at which bottom of hero comes into viewport (adjusted for border height)
+        let trigger = hero.scrollHeight - w.innerHeight + header.scrollHeight;
+        //increase in window pageYOffset beyond trigger point, as a proportion of the window height
         let offset = (w.pageYOffset - trigger) / w.innerHeight;
-        let change = 50 - (+offset * 100);
+        //amount by which to reduce the top percentage
+        let change = 50 - (offset * 100);
         if(w.pageYOffset > trigger) {
             mainTitle.style.top = `${change}%`;
-            mainTitle.style.transition = "top 0s linear";
+            mainTitle.style.transition = "top 0s linear"; //to mimic a scroll effect
 
             intro.style.top = `${change}%`;
             intro.style.transition = "top 0s linear";
 
             scroll.style.display = "none";
-        } else if (w.pageYOffset > w.innerHeight) {
-            mainTitle.style.top = "50%";
-            mainTitle.style.transition = "top 0s linear";
 
-            intro.style.top = "50%";
-            intro.style.transition = "top 0s linear";
-
-            scroll.style.display = "inline-block";
+        } else {
+            mainTitle.style.top = "";
+            intro.style.top = "";
         }
     })
 
