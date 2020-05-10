81be60f09be8145af32dfab1fad54fc9e53e6ce1
Harriet Goddard <goddardhe@gmail.com>
2020-05-10 13:40:48 +0100

    scroll display fixed

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -69,18 +69,18 @@
         let offset = (w.pageYOffset - trigger) / w.innerHeight;
         //amount by which to reduce the top percentage. 0.75 slows for parallax effect
         let change = 50 - (offset * 100 * 0.75);
+
         if(w.pageYOffset > trigger) {
             mainTitle.style.top = `${change}%`;
-            mainTitle.style.transition = "top 0s linear"; //to mimic a scroll effect
 
             intro.style.top = `${change}%`;
-            intro.style.transition = "top 0s linear";
 
             scroll.style.display = "none";
 
         } else {
             mainTitle.style.top = "";
             intro.style.top = "";
+            scroll.style.display = "inline-block";
         }
     })
 
