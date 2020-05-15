34434700c7fdb4d2cb381489a28ca47f02df07ac
Harriet Goddard <goddardhe@gmail.com>
2020-05-15 21:16:40 +0100

    scroll on mobile smoothed out

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -31,15 +31,6 @@
                     "width": "calc(100% - 2 * var(--border)",
                     "transition": "width 1.10s",
                 });
-            
-            } else {
-
-                setStyle(wiper, {
-                    "width": "calc(100% - var(--border)",
-                    "transition": "width 1.10s",
-                });
-
-            }
 
                 setStyle(mainTitle, {
                     "right": "calc(0.17 * var(--border))",
@@ -55,6 +46,27 @@
                     "right": "calc(0.8 * var(--border))",
                     "transition" : "right 1s"
                 });
+            
+            } else {
+
+                // setStyle(wiper, {
+                //     "width": "calc(100% - var(--border)",
+                //     "transition": "width 1.10s",
+                // });
+
+                setStyle(wiper, {
+                    "transform": "translateX(calc(50% - var(--border)))",
+                    "transition": "transform 1.10s"
+                });
+
+                setStyle(preTitle, {
+                    "opacity" : "0",
+                    "transition" : "opacity 1s"
+                })
+
+            }
+
+
 
             // } else {
 
@@ -82,37 +94,49 @@
             mainTitle.style.right = "";
             scroll.style.right = "";
             preTitle.style.top = "";
+            preTitle.style.opacity = "";
             intro.style.opacity = "";
             intro.style.transition = "1s";
             navMain.style.opacity = "";
             navMain.style.transition = "1s";
+
+            wiper.style.transform = "";
         }
 
     })
     
     w.addEventListener("scroll", () => {
-        //window pageYOffset point at which bottom of hero comes into viewport (adjusted for border height)
-        let trigger = screen.width > 768 ? hero.scrollHeight - w.innerHeight + header.scrollHeight : hero.scrollHeight - w.innerHeight;
-        //increase in window pageYOffset beyond trigger point, as a proportion of the window height
-        let offset = (w.pageYOffset - trigger) / w.innerHeight;
-        //amount by which to reduce the top percentage. 0.75 slows for parallax effect
-        let change = screen.width > 768 ? 50 - (offset * 100 * 0.77) : 50 - (offset * 100);
-        let introChange = screen.width > 768 ? 50 - (offset * 100 * 0.77) : 80 - (offset * 100);
-        // let introChange = screen.width > 768 ? change : 50 + change;
+        
+        if(screen.width > 768 ) {
+            //window pageYOffset point at which bottom of hero comes into viewport (adjusted for border height)
+            let trigger = screen.width > 768 ? hero.scrollHeight - w.innerHeight + header.scrollHeight : hero.scrollHeight - w.innerHeight;
+            //increase in window pageYOffset beyond trigger point, as a proportion of the window height
+            let offset = (w.pageYOffset - trigger) / w.innerHeight;
+            //amount by which to reduce the top percentage. 0.75 slows for parallax effect
+            let change = screen.width > 768 ? 50 - (offset * 100 * 0.77) : 50 - (offset * 100);
+            let introChange = screen.width > 768 ? 50 - (offset * 100 * 0.77) : 80 - (offset * 100);
+            // let introChange = screen.width > 768 ? change : 50 + change;
 
-        if(w.pageYOffset > trigger) {
-            
-            // if (screen.width > 768) {
-                mainTitle.style.top = `${change}%`;
-                intro.style.top = `${introChange}%`;
-            // }
+            if(w.pageYOffset > trigger) {
+                
+                // if (screen.width > 768) {
+                    mainTitle.style.top = `${change}%`;
+                    intro.style.top = `${introChange}%`;
+                // }
 
+                scroll.style.display = "none";
+
+            } else {
+                mainTitle.style.top = "";
+                intro.style.top = "";
+                scroll.style.display = "inline-block";
+            }
+        }
+    })
+
+    w.addEventListener("resize", () => {
+        if (screen.width < 768) {
             scroll.style.display = "none";
-
-        } else {
-            mainTitle.style.top = "";
-            intro.style.top = "";
-            scroll.style.display = "inline-block";
         }
     })
 
@@ -120,5 +144,16 @@
     window.onbeforeunload = () => {
         window.scrollTo(0, 0);
     }
+
+    w.addEventListener("touchmove", () => {
+        if(w.pageYOffset > 0) {
+            setStyle(wiper, {
+                "transform": "translateX(calc(50% - var(--border)))",
+                "transition": "transform 1.10s"
+            });
+        } else {
+            wiper.style.transform = "";
+        }
+    })
 
 })(document, window);
